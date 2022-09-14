564b257de8e192e2cdfea80d753166bfcd3b940c
imbev <imbev@protonmail.com>
2022-09-14 17:23:12 +0000

    Minor refactor of app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv; load_dotenv()
 import os
 
 from app.website import website_blueprint
+from app.model import db
 
 
 def create_app():
@@ -10,12 +11,10 @@ def create_app():
 
     app.config['HOST'] = os.getenv('HOST')
     app.config['ADMIN_PASSWORD'] = os.getenv('ADMIN_PASSWORD')
-    app.config['DATABASE'] = os.getenv('DATABASE')
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{app.config["DATABASE"]}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getenv("DATABASE")}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    from app.model import db
     db.init_app(app)
 
     app.register_blueprint(website_blueprint)
