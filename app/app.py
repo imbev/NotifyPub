efa7057aa898715aa6ab8c6cfe192a2c06fd314e
imbev <imbev@protonmail.com>
2022-09-15 13:16:21 +0000

    Add title config option and move host config option

--- app/app.py
+++ app/app.py
@@ -9,11 +9,16 @@ from app.model import db
 def create_app():
     app = Flask(__name__)
 
-    app.config['HOST'] = os.getenv('HOST')
     app.config['ADMIN_PASSWORD'] = os.getenv('ADMIN_PASSWORD')
     
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getenv("DATABASE")}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    app.config['WEBSITE'] = {
+        'base_url': os.getenv('HOST'),
+        'title': os.getenv('TITLE')
+    }
 
     db.init_app(app)
 
