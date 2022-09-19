4f5d465b1c8b082622e19bd3dde784427e427697
imbev <imbev@protonmail.com>
2022-09-19 16:46:06 +0000

    Query db for most recent notif and token in dashboard

--- app/website.py
+++ app/website.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select, desc
 from flask import Blueprint, current_app, render_template, redirect, url_for
 from flask_login import login_required
 from app.login import login_manager
@@ -11,7 +11,7 @@ conn = engine.connect()
 
 website = Blueprint('website', __name__)
 
-engine = create_engine(os.getenv("DATABASE"))
+engine = create_engine(os.getenv("DATABASE"), connect_args={'check_same_thread': False})
 meta.create_all(engine)
 conn = engine.connect()
 
@@ -24,9 +24,19 @@ def index():
 def dashboard():
     published_count = 0
     tokens_count = 0
+    try:
+        recent_published = conn.execute(select(messages).order_by(desc(messages.c.time_created))).all()[0]
+    except IndexError:
+        recent_published = ""
+    try:
+        recent_token = conn.execute(select(tokens).order_by(desc(tokens.c.time_created))).all()[0]
+    except IndexError:
+        recent_token = ""
     return render_template(
         'dashboard.html',
          config=current_app.config['WEBSITE'], 
          published_count=published_count, 
-         tokens_count=tokens_count
+         tokens_count=tokens_count,
+         recent_published=recent_published,
+         recent_token=recent_token
     )
