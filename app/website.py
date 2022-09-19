462654ca75953153a5ceab0c884e36b79e9b491b
imbev <imbev@protonmail.com>
2022-09-19 18:03:27 +0000

    Only use conn via with and remove duplicate engine

--- app/website.py
+++ app/website.py
@@ -9,15 +9,10 @@ from wtforms.validators import DataRequired
 from app.login import login_manager
 from app.model import meta, messages, tokens
 
-engine = create_engine(os.getenv("DATABASE"))
-meta.create_all(engine)
-conn = engine.connect()
-
 website = Blueprint('website', __name__)
 
 engine = create_engine(os.getenv("DATABASE"), connect_args={'check_same_thread': False})
 meta.create_all(engine)
-conn = engine.connect()
 
 @website.route("/")
 def index():
@@ -26,8 +21,9 @@ def index():
 @website.route('/dashboard')
 @login_required
 def dashboard():
-    published = conn.execute(select(messages).order_by(desc(messages.c.time_created))).all()
-    tokens_created = conn.execute(select(tokens).order_by(desc(tokens.c.time_created))).all()
+    with engine.begin() as conn:
+        published = conn.execute(select(messages).order_by(desc(messages.c.time_created))).all()
+        tokens_created = conn.execute(select(tokens).order_by(desc(tokens.c.time_created))).all()
     recent_published = published[0] if published else ""
     recent_token = tokens_created[0] if tokens_created else ""
     return render_template(
