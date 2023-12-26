1a0d3632fb476f785ff3ffb2a9a2f03840eb744f
Isaac Beverly <imbev@protonmail.com>
2023-12-26 15:05:21 +0000

    Add models for Message, Token

--- notifypub/app/models.py
+++ notifypub/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Message(models.Model):
+    title = models.CharField()
+    summary = models.CharField()
+    content = models.CharField()
+    create_date = models.DateTimeField("date created")
+
+
+class Token(models.Model):
+    description = models.CharField()
+    token = models.CharField()
+    create_date = models.DateTimeField("date created")
